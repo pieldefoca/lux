f91d3eeb7ffe0a6d9889d2c9b5df1547a7aabda1
pieldefoca <betibaskonia@gmail.com>
2024-03-12 15:12:40 +0100

    Update rich-text.blade.php

--- resources/views/components/input/rich-text.blade.php
+++ resources/views/components/input/rich-text.blade.php
@@ -4,10 +4,36 @@
     'toolbar' => 'h1 h2 h3 bold italic underline align | bullist numlist | image media link table | fullscreen',
 ])
 
+@php
+$wireModelData = $attributes->whereStartsWith('wire:model')->getAttributes();
+$wireModelName = array_key_first($wireModelData);
+$wireModelValue = $wireModelData[$wireModelName];
+$id = str($wireModelValue)->replace('.', '_')->toString();
+$modifiers = array_slice(explode('.', $wireModelName), 1);
+$live = in_array('live', $modifiers);
+$blur = in_array('blur', $modifiers);
+$debounce = in_array('debounce', $modifiers);
+$debounceMs = null;
+if($debounce) {
+    $index = array_search('debounce', $modifiers);
+    try {
+        $possibleDebounceMs = $modifiers[$index + 1];
+        if(str($possibleDebounceMs)->contains('ms')) {
+            $debounceMs = $possibleDebounceMs;
+        }
+    } catch(\Exception $e) {}
+}
+$xModel = str('x-model')
+    ->when(!$live, fn($str) => $str->append('.lazy'))
+    ->when($live || $debounce, fn($str) => $str->append('.debounce'))
+    ->when(!is_null($debounceMs), fn($str) => $str->append(".{$debounceMs}"))
+    ->append('=value');
+@endphp
+
 <div class="w-full">
     <script src="/vendor/lux/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
 
-    @php
+    {{-- @php
         $wireModel = $attributes->whereStartsWith('wire:model')->getAttributes();
         $model = '';
         foreach($wireModel as $attribute => $value) {
@@ -19,20 +45,47 @@
         $locales = $translatable
             ? Pieldefoca\Lux\Models\Locale::all()
             : [Pieldefoca\Lux\Models\Locale::default()];
-    @endphp
+    @endphp --}}
 
     <div
         x-data="{
             locale: @entangle('locale'),
             id: '',
+            value: null,
+            field: @js($wireModelValue),
+            live: @js($live),
+            blur: @js($blur),
             translatable: @js($translatable),
-            model: '{{$model}}',
+            {{-- model: '{{$model}}', --}}
             init() {
+                field = this.translatable ? `${this.field}.${$store.lux.locale}` : this.field
+                this.value = $wire.$get(field)
+
+                $wire.$watch(field, (value) => {
+                    tinymce.get(this.id).setContent(value)
+                })
+
+                this.$watch('value', value => {
+                    this.sync()
+                })
+
+                this.$watch('$store.lux.locale', (locale) => {
+                    field = this.translatable ? `${this.field}.${locale}` : this.field
+                    this.value = $wire.$get(field)
+                    tinymce.get(this.id).setContent(this.value)
+                })
+
+                this.$nextTick(() => {
+                    if(this.$refs.inlineLeadingAddon) {
+                        width = this.$refs.inlineLeadingAddon.getBoundingClientRect().width
+                        this.$refs.input.style.paddingLeft = `calc(${width}px + 1.8rem)`
+                    }
+                })
                 this.id = '{{$id}}'
                 if(this.translatable) {
                     this.model += `.${this.locale}`
                 }
-                
+
                 tinymce.init({
                     selector: `#${this.id}`,
                     language: 'es',
@@ -41,10 +94,10 @@
                     menubar: false,
                     toolbar: @js($toolbar),
                     init_instance_callback: (editor) => {
-                        editor.setContent($wire.$get(`${this.model}`) || '')
+                        editor.setContent(this.value)
 
                         editor.on('blur', (e) => {
-                            $wire.$set(`${this.model}`, editor.getContent())
+                            this.value = editor.getContent()
                         });
                     },
                     images_upload_url: '/admin/tinymce/upload',
@@ -90,17 +143,10 @@
                         xhr.send(formData);
                     }),
                 })
-
-                $wire.$watch('{{$model}}', () => {
-                    tinymce.get(this.id).setContent($wire.$get(`${this.model}`) || '')
-                })
-
-                $watch('locale', ($value) => {
-                    if(this.translatable) {
-                        this.model = `{{$model}}.${this.locale}`
-                    }
-                    tinymce.get(this.id).setContent($wire.$get(`${this.model}`) || '')
-                })
+            },
+            sync() {
+                field = this.translatable ? `${this.field}.${$store.lux.locale}` : this.field
+                $wire.$set(field, this.value, this.live || this.blur)
             },
         }"
         @class([
