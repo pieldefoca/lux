1281db8769e161ccf7f6dc603c673978851f2af8
David <betibaskonia@gmail.com>
2024-06-05 07:34:57 +0200

    Remove page images

--- resources/views/livewire/pages/index.blade.php
+++ resources/views/livewire/pages/index.blade.php
@@ -4,9 +4,7 @@
     <div class="my-12 mx-auto space-y-4 max-w-4xl">
         @foreach($pages as $page)
             <a href="{{ route('lux.pages.edit', $page) }}" class="flex items-center justify-between w-full border border-gray-300 bg-white p-2 pr-6 rounded-lg transition-all duration-300 hover:shadow-lg">
-                <div class="flex items-center space-x-6">
-                    <img src="{{ $page->screenshotUrl }}" class="w-36 aspect-video rounded-md"  alt="{{ $page->title }}"/>
-                    
+                <div class="flex items-center space-x-6">                    
                     <div class="space-y-3">
                         <p class="text-xl font-bold">{{ $page->name }}</p>
                         <p class="text-gray-400">{{ $page->localizedUrl($this->locale) }}</p>
